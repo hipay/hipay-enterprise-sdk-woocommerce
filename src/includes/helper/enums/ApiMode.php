1a418a4499f72bffc9b0d1620aecaa319f956bf3
BERTHELOT Aymeric <lemaildeaymeric@gmail.com>
2018-11-14 09:22:13 +0100

    Rework

---
+++ src/includes/helper/enums/ApiMode.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * HiPay Enterprise SDK Prestashop
+ *
+ * 2017 HiPay
+ *
+ * NOTICE OF LICENSE
+ *
+ * @author    HiPay <support.tpp@hipay.com>
+ * @copyright 2017 HiPay
+ * @license   https://github.com/hipay/hipay-enterprise-sdk-prestashop/blob/master/LICENSE.md
+ */
+
+/**
+ *
+ * @author      HiPay <support.tpp@hipay.com>
+ * @copyright   Copyright (c) 2017 - HiPay
+ * @license     https://github.com/hipay/hipay-enterprise-sdk-prestashop/blob/master/LICENSE.md
+ * @link    https://github.com/hipay/hipay-enterprise-sdk-prestashop
+ */
+class ApiMode
+{
+    const DIRECT_POST = 'direct_post';
+
+    const HOSTED_PAGE = 'hosted';
+
+    const HOSTED_PAGE_IFRAME = 'iframe';
+}
---
+++ src/includes/helper/enums/ThreeDS.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * HiPay Enterprise SDK Prestashop
+ *
+ * 2017 HiPay
+ *
+ * NOTICE OF LICENSE
+ *
+ * @author    HiPay <support.tpp@hipay.com>
+ * @copyright 2017 HiPay
+ * @license   https://github.com/hipay/hipay-enterprise-sdk-prestashop/blob/master/LICENSE.md
+ */
+
+/**
+ *
+ * @author      HiPay <support.tpp@hipay.com>
+ * @copyright   Copyright (c) 2017 - HiPay
+ * @license     https://github.com/hipay/hipay-enterprise-sdk-prestashop/blob/master/LICENSE.md
+ * @link    https://github.com/hipay/hipay-enterprise-sdk-prestashop
+ */
+class ThreeDS
+{
+    const THREE_D_S_DISABLED = 0;
+
+    const THREE_D_S_TRY_ENABLE_ALL = 1;
+
+    const THREE_D_S_TRY_ENABLE_RULES = 2;
+
+    const THREE_D_S_FORCE_ENABLE_ALL = 3;
+
+    const THREE_D_S_FORCE_ENABLE_RULES = 4;
+}
