1bd55c565e9c45b8c1b6281b01ec0b7043dd4dc6
BERTHELOT Aymeric <lemaildeaymeric@gmail.com>
2018-11-15 09:28:54 +0100

    Add sample data

--- bin/docker/images/woocommerce/setup-woocommerce.sh
+++ bin/docker/images/woocommerce/setup-woocommerce.sh
@@ -1,37 +1,59 @@
 #!/usr/bin/env bash
+COLOR_SUCCESS='\033[0;32m'
+NC='\033[0m'
+
 sleep 10
 /bin/bash /tmp/docker-entrypoint.sh "apache2-foreground"
-
 sleep 20
-wp core install --url=http://localhost:8000 --title="Test Wordpress" --admin_user=$ADMIN_USERNAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path="/var/www/html"
-wp plugin install woocommerce --version=$WOOCOMMERCE_VERSION --allow-root --activate --path="/var/www/html"
-wp theme install storefront --allow-root --activate --path="/var/www/html"
-wp plugin install wordpress-importer --activate --allow-root --path="/var/www/html"
-php -f /tmp/setup-wizard-woocommerce.php
 
-cp -R /tmp/woocommerce_hipayenterprise /var/www/html/wp-content/plugins
+ #if [ ! -f /var/www/html/wp-content/plugins/woocommerce/woocommerce.php  ]; then
 
-printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
-printf "\n${COLOR_SUCCESS}            INSTALLATION SDK PHP         ${NC}\n"
-printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    printf "\n${COLOR_SUCCESS}            INSTALL WOOCOMMERCE           ${NC}\n"
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    wp core install --url=http://localhost:8000 --title="Test Wordpress" --admin_user=$ADMIN_USERNAME --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path="/var/www/html"
+    wp plugin install woocommerce --version=$WOOCOMMERCE_VERSION --allow-root --activate --path="/var/www/html"
+    wp theme install storefront --allow-root --activate --path="/var/www/html"
+    wp plugin install wordpress-importer --activate --allow-root --path="/var/www/html"
+    php -f /tmp/setup-wizard-woocommerce.php
 
-cd /var/www/html/wp-content/plugins/woocommerce_hipayenterprise/ \
-&& composer install --no-dev
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    printf "\n${COLOR_SUCCESS}            COPY  MODULE FILE            ${NC}\n"
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    cp -R /tmp/woocommerce_hipayenterprise /var/www/html/wp-content/plugins
 
-wp import /var/www/html/wp-content/plugins/woocommerce/sample-data/smaple_product.csv --authors=create --path="/var/www/html"
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    printf "\n${COLOR_SUCCESS}            INSTALLATION SDK PHP         ${NC}\n"
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    cd /var/www/html/wp-content/plugins/woocommerce_hipayenterprise/ \
+    && composer install --no-dev
+
+    #==========================================
+    # Import sample data
+    #==========================================
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    printf "\n${COLOR_SUCCESS}            IMPORT SAMPLE DATA           ${NC}\n"
+    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+    wp import /var/www/html/wp-content/plugins/woocommerce/sample-data/sample_products.xml --allow-root --authors=create --path="/var/www/html"
+    sleep 10
+
+    #==========================================
+    # Install XDebug
+    #==========================================
+    if [ "$XDEBUG_ENABLED" = "1" ]; then
+        printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+        printf "\n${COLOR_SUCCESS}     INSTALLATION XDEBUG $ENVIRONMENT    ${NC}\n"
+        printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
+
+        echo '' && pecl install xdebug-2.6.0
+        echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
+        echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini
+        echo "xdebug.remote_autostart=off" >> /usr/local/etc/php/conf.d/xdebug.ini
+    fi
+
+#fi
 
 #==========================================
-# Install XDebug
+# APACHE RUNNING
 #==========================================
-if [ "$XDEBUG_ENABLED" = "1" ]; then
-    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
-    printf "\n${COLOR_SUCCESS}     INSTALLATION XDEBUG $ENVIRONMENT    ${NC}\n"
-    printf "\n${COLOR_SUCCESS} ======================================= ${NC}\n"
-
-    echo '' && pecl install xdebug-2.6.0
-    echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_autostart=off" >> /usr/local/etc/php/conf.d/xdebug.ini
-fi
-
 exec apache2 -DFOREGROUND
