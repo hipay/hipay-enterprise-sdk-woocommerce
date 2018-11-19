dd7f977b0a9df51bdd114b9b07e0bbc0dec6c549
Landais Etienne <elandais@hipay.com>
2018-11-19 10:49:56 +0100

    Rework logs

--- src/woocommerce_hipayenterprise/includes/helper/class-hipay-log.php
+++ src/woocommerce_hipayenterprise/includes/helper/class-hipay-log.php
@@ -1,39 +1,48 @@
 <?php
-if (! defined('ABSPATH')) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
+/**
+ * Class Hipay_Log
+ * @TODO : Créer un custom handler
+ */
 class Hipay_Log
 {
-    const LOG_HIPAY_ERROR = "error";
-
-    const LOG_HIPAY_INFOS = "infos";
-
-    const LOG_HIPAY_REQUEST = "request";
-
-    const LOG_HIPAY_CALLBACK = "callback";
-
-    private $basePath;
 
     private $plugin;
 
+    private $logger;
+
     const DEBUG_KEYS_MASK = '****';
 
-    private $privateDataKeys = array('token', 'cardtoken', 'card_number', 'cvc', 'api_password_sandbox',
-        'api_tokenjs_username_sandbox', 'api_tokenjs_password_publickey_sandbox', 'api_secret_passphrase_sandbox',
-        'api_password_production', 'api_tokenjs_username_production',
-        'api_tokenjs_password_publickey_production', 'api_secret_passphrase_production', 'api_moto_username_production',
-        'api_moto_password_production', 'api_moto_secret_passphrase_production');
+    private $privateDataKeys = array(
+        'token',
+        'cardtoken',
+        'card_number',
+        'cvc',
+        'api_password_sandbox',
+        'api_tokenjs_username_sandbox',
+        'api_tokenjs_password_publickey_sandbox',
+        'api_secret_passphrase_sandbox',
+        'api_password_production',
+        'api_tokenjs_username_production',
+        'api_tokenjs_password_publickey_production',
+        'api_secret_passphrase_production',
+        'api_moto_username_production',
+        'api_moto_password_production',
+        'api_moto_secret_passphrase_production'
+    );
 
     /**
-     *
+     * Hipay_Log constructor.
+     * @param $plugin
      */
     public function __construct($plugin)
     {
         $this->plugin = $plugin;
-        $this->basePath = WC_HIPAYENTERPRISE_PATH . 'logs/';
+        $this->logger = wc_get_logger();
     }
-
 
     /**
      *  Log exception
@@ -53,7 +62,7 @@ class Hipay_Log
      */
     public function logErrors($msg)
     {
-        $this->writeLogs(self::LOG_HIPAY_ERROR, $this->getExecutionContext() . ':' . $msg);
+        $this->logger->error($this->getExecutionContext() . ':' . $msg, array('source' => $this->plugin->id));
     }
 
     /**
@@ -63,13 +72,11 @@ class Hipay_Log
      */
     public function logInfos($msg)
     {
-        //if ($this->module->hipayConfigTool->getPaymentGlobal()["log_infos"]) {
-            if (is_array($msg)) {
-                $this->writeLogs(self::LOG_HIPAY_INFOS, print_r($this->filterDebugData($msg), true));
-            } else {
-                $this->writeLogs(self::LOG_HIPAY_INFOS, $msg);
-            }
-        //}
+        if (is_array($msg)) {
+            $this->logger->info(print_r($this->filterDebugData($msg), true), array('source' => $this->plugin->id));
+        } else {
+            $this->logger->info($msg, array('source' => $this->plugin->id));
+        }
     }
 
     /**
@@ -79,9 +86,9 @@ class Hipay_Log
      */
     public function logCallback($transaction)
     {
-        $this->writeLogs(
-            self::LOG_HIPAY_CALLBACK,
-            print_r($this->filterDebugData($this->toArray($transaction)), true)
+        $this->logger->debug(
+            print_r($this->filterDebugData($this->toArray($transaction)), true),
+            array('source' => $this->plugin->id)
         );
     }
 
@@ -92,31 +99,10 @@ class Hipay_Log
      */
     public function logRequest($request)
     {
-        $this->writeLogs(self::LOG_HIPAY_REQUEST, print_r($this->filterDebugData($this->toArray($request)), true));
-    }
-
-    /**
-     * Format log message and write log in system file
-     *
-     * @param $type string
-     * @param $message string
-     * @return bool|int
-     */
-    private function writeLogs($type, $message)
-    {
-        $formatted_message = date('Y/m/d - H:i:s') . ': ' . $message . "\r\n";
-        return file_put_contents($this->getFilename($type), $formatted_message, FILE_APPEND);
-    }
-
-    /**
-     * Get log filename according  type of error
-     *
-     * @param $type string
-     * @return string
-     */
-    private function getFilename($type)
-    {
-        return $this->basePath . date('Y-m-d') . '-' . 'hipay' . '-' . $type . '.log';
+        $this->logger->debug(
+            print_r($this->filterDebugData($this->toArray($request)), true),
+            array('source' => $this->plugin->id)
+        );
     }
 
     /**
@@ -153,16 +139,5 @@ class Hipay_Log
             return $debug[2]['class'] . ':' . $debug[2]['function'];
         }
         return null;
-    }
-
-    /**
-     *  Convert Object to Array
-     *
-     * @param $object
-     * @return array
-     */
-    private function toArray($object)
-    {
-        return (array)$object;
     }
 }
