dabe73ff5ca157d7d2ca4ece8b506528576ca7d4
Landais Etienne <elandais@hipay.com>
2018-11-21 11:07:58 +0100

    Init CI

--- wordpress.sh
+++ wordpress.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if [ "$1" = 'init' ] && [ "$2" = '' ];then
-     docker-compose -f docker-compose-dev.yml stop
-     docker-compose -f docker-compose-dev.yml rm -fv
+     docker-compose -f docker-compose.dev.yml stop
+     docker-compose -f docker-compose.dev.yml rm -fv
      rm -Rf wordpress/
      rm -Rf data/
-     docker-compose -f docker-compose-dev.yml build --no-cache
-     docker-compose -f docker-compose-dev.yml up -d
+     docker-compose -f docker-compose.dev.yml build --no-cache
+     docker-compose -f docker-compose.dev.yml up -d
 fi
 
 if [ "$1" = 'restart' ];then
-     docker-compose -f docker-compose-dev.yml  stop
-     docker-compose -f docker-compose-dev.yml  up -d
+     docker-compose -f docker-compose.dev.yml  stop
+     docker-compose -f docker-compose.dev.yml  up -d
 fi
 
 if [ "$1" = 'l' ];then
