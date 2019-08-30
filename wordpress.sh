eb79184f0d8bfaa3302378537301eb4c9cc4384a
Jean-Baptiste Louvet <jlouvet@hipay.com>
2019-08-30 17:11:08 +0200

    EC-782 : Corrected git hooks

--- wordpress.sh
+++ wordpress.sh
@@ -3,39 +3,33 @@
 manageComposerForData() {
     COMPOSER_JSON_FILE="src/woocommerce_hipayenterprise/composer.json"
 
-    if [ ! -f .git/hooks/pre-commit ]
-    then
-        echo "Setting up git pre-commit hook..."
+    echo "Setting up git pre-commit hook..."
 
-        echo "#!/bin/bash" > .git/hooks/pre-commit
-        echo "COMPOSER_JSON_FILE='"$COMPOSER_JSON_FILE"'" >> .git/hooks/pre-commit
-        echo "git status --porcelain -uno | grep \$COMPOSER_JSON_FILE" >> .git/hooks/pre-commit
-        echo "if [ $? -eq 0 ]" >> .git/hooks/pre-commit
-        echo "then" >> .git/hooks/pre-commit
-        echo "    cp \$COMPOSER_JSON_FILE \$COMPOSER_JSON_FILE.bak" >> .git/hooks/pre-commit
-        echo "    cat \$COMPOSER_JSON_FILE.bak | python -c \"import sys, json; composerObj=json.load(sys.stdin); del composerObj['scripts']; print json.dumps(composerObj, False, True, True, True, None, 2);\" > \$COMPOSER_JSON_FILE" >> .git/hooks/pre-commit
-        echo "    git add \$COMPOSER_JSON_FILE" >> .git/hooks/pre-commit
-        echo "fi" >> .git/hooks/pre-commit
-        echo "exit 0" >> .git/hooks/pre-commit
+    echo "#!/bin/bash" > .git/hooks/pre-commit
+    echo "COMPOSER_JSON_FILE='"$COMPOSER_JSON_FILE"'" >> .git/hooks/pre-commit
+    echo "git status --porcelain -uno | grep \$COMPOSER_JSON_FILE" >> .git/hooks/pre-commit
+    echo "if [ $? -eq 0 ]" >> .git/hooks/pre-commit
+    echo "then" >> .git/hooks/pre-commit
+    echo "    cp \$COMPOSER_JSON_FILE \$COMPOSER_JSON_FILE.bak" >> .git/hooks/pre-commit
+    echo "    cat \$COMPOSER_JSON_FILE.bak | python -c \"import sys, json; composerObj=json.load(sys.stdin); composerObj['scripts'] = None; del composerObj['scripts']; print json.dumps(composerObj, False, True, True, True, None, 2);\" > \$COMPOSER_JSON_FILE" >> .git/hooks/pre-commit
+    echo "    git add \$COMPOSER_JSON_FILE" >> .git/hooks/pre-commit
+    echo "fi" >> .git/hooks/pre-commit
+    echo "exit 0" >> .git/hooks/pre-commit
 
-        chmod 775 .git/hooks/pre-commit
-    fi
+    chmod 775 .git/hooks/pre-commit
 
-    if [ ! -f .git/hooks/post-commit ]
-    then
-        echo "Setting up git post-commit hook..."
+    echo "Setting up git post-commit hook..."
 
-        echo "#!/bin/bash" > .git/hooks/post-commit
-        echo "COMPOSER_JSON_FILE='"$COMPOSER_JSON_FILE"'" >> .git/hooks/post-commit
-        echo "if [ -f \$COMPOSER_JSON_FILE.bak ]" >> .git/hooks/post-commit
-        echo "then" >> .git/hooks/post-commit
-        echo "    cp \$COMPOSER_JSON_FILE.bak \$COMPOSER_JSON_FILE" >> .git/hooks/post-commit
-        echo "    rm \$COMPOSER_JSON_FILE.bak" >> .git/hooks/post-commit
-        echo "fi" >> .git/hooks/post-commit
-        echo "exit 0" >> .git/hooks/post-commit
+    echo "#!/bin/bash" > .git/hooks/post-commit
+    echo "COMPOSER_JSON_FILE='"$COMPOSER_JSON_FILE"'" >> .git/hooks/post-commit
+    echo "if [ -f \$COMPOSER_JSON_FILE.bak ]" >> .git/hooks/post-commit
+    echo "then" >> .git/hooks/post-commit
+    echo "    cp \$COMPOSER_JSON_FILE.bak \$COMPOSER_JSON_FILE" >> .git/hooks/post-commit
+    echo "    rm \$COMPOSER_JSON_FILE.bak" >> .git/hooks/post-commit
+    echo "fi" >> .git/hooks/post-commit
+    echo "exit 0" >> .git/hooks/post-commit
 
-        chmod 775 .git/hooks/post-commit
-    fi
+    chmod 775 .git/hooks/post-commit
 }
 
 manageComposerForData
