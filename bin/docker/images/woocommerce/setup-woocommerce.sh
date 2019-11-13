7c80ce3af3c3c9a710b9ecddbb91363534428884
Landais Etienne <elandais@hipay.com>
2019-11-13 14:38:55 +0100

    Fix CI

--- bin/docker/images/woocommerce/setup-woocommerce.sh
+++ bin/docker/images/woocommerce/setup-woocommerce.sh
@@ -9,7 +9,7 @@ ENV_PROD="production"
 maxcounter=45
 counter=1
 while ! mysql --protocol TCP -h $WORDPRESS_DB_HOST -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -e "show databases;" > /dev/null 2>&1; do
-    sleep 1
+    sleep 5
     counter=`expr $counter + 1`
     if [ $counter -gt $maxcounter ]; then
         >&2 echo "We have been waiting for MySQL too long already; failing."
