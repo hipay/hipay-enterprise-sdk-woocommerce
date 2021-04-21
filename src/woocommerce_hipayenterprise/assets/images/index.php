fb038ec7048acdc1655d51aa324b587d78b4e082
Pedro CAVACO <pcavaco@hipay.com>
2021-04-21 10:57:30 +0100

    added mbway and multibanco payment methods

--- src/woocommerce_hipayenterprise/assets/images/index.php
+++ src/woocommerce_hipayenterprise/assets/images/index.php
@@ -1,1 +1,22 @@
 <?php
+/**
+ * HiPay Enterprise SDK WooCommerce
+ *
+ * 2018 HiPay
+ *
+ * NOTICE OF LICENSE
+ *
+ * @author    HiPay <support.tpp@hipay.com>
+ * @copyright 2018 HiPay
+ * @license   https://github.com/hipay/hipay-enterprise-sdk-woocommerce/blob/master/LICENSE.md
+ */
+
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+header("Location: ../");
+exit;
