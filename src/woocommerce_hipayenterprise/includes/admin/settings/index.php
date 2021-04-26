00a40f176339a215c7b3e3cf8e4592d105e4b632
Aymeric BERTHELOT <aberthelot@hipay.com>
2021-04-26 18:07:33 +0200

    Add index.php

--- src/woocommerce_hipayenterprise/includes/admin/settings/index.php
+++ src/woocommerce_hipayenterprise/includes/admin/settings/index.php
@@ -11,12 +11,4 @@
  * @license   https://github.com/hipay/hipay-enterprise-sdk-woocommerce/blob/master/LICENSE.md
  */
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
-header("Location: ../");
 exit;
