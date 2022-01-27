4ab406c282c59e11e7c534c045f1a0490ab2d73d
Theo Viardin <tviardin@hipay.com>
2022-01-27 17:31:20 +0100

    Test

--- bin/docker/images/woocommerce/setup-woocommerce.sh
+++ bin/docker/images/woocommerce/setup-woocommerce.sh
@@ -8,7 +8,7 @@ ENV_PROD="production"
 # wait until MySQL is really available
 maxcounter=45
 counter=1
-while ! mysql --protocol TCP -h $WORDPRESS_DB_HOST -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -e "show databases; " >/dev/null 2>&1; do
+while ! mysql --protocol TCP -h $WORDPRESS_DB_HOST -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -e "show databases;" >/dev/null 2>&1; do
     sleep 5
     counter=$(expr $counter + 1)
     if [ $counter -gt $maxcounter ]; then
