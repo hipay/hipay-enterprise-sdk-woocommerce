ab4bda1ff9870c5e366fb295f7827526506b7caf
Adam BRECHET <abrechet@hipay.com>
2023-03-10 15:39:49 +0100

    Upgrade woocommerce version

--- wordpress.sh
+++ wordpress.sh
@@ -38,6 +38,7 @@ manageComposerForData
 
 if [ "$1" = 'init' ] && [ "$2" = '' ]; then
      docker compose -f docker-compose.dev.yml rm -sfv
+     docker compose -f docker-compose.dev.yml rm -sfv database
      sudo rm -Rf wordpress/core/ data/ src/woocommerce_hipayenterprise/vendor/ src/woocommerce_hipayenterprise/composer.lock
      docker compose -f docker-compose.dev.yml up -d --build
 fi
