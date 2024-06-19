2f97b96fb38104acccd3d6112c4d90bf0945b694
Adam BRECHET <abrechet@hipay.com>
2024-06-19 16:18:16 +0200

    Update readonly system as prestashop module

--- src/woocommerce_hipayenterprise/assets/js/admin/hipay-admin.js
+++ src/woocommerce_hipayenterprise/assets/js/admin/hipay-admin.js
@@ -1,79 +1,71 @@
 jQuery(function ($) {
-    'use strict';
+  'use strict';
 
-    $('#operating_mode').change(function () {
-        if ($(this).val() == "hosted_page") {
-            $(".hosted_page_config").removeClass("hidden").show();
-            $(".directPost_page_config").hide();
+  $('#operating_mode').change(function () {
+    if ($(this).val() == 'hosted_page') {
+      $('.hosted_page_config').removeClass('hidden').show();
+      $('.directPost_page_config').hide();
+    } else {
+      $('.directPost_page_config').removeClass('hidden').show();
+      $('.hosted_page_config').hide();
+    }
+    return false;
+  });
+
+  $('ul.nav-tabs > li > a').on('shown.bs.tab', function (e) {
+    var id = $(e.target).attr('href').substr(1);
+    window.location.hash = id;
+  });
+
+  var hash = window.location.hash;
+  $('#hipay-container-admin ul a[href="' + hash + '"]').tab('show');
+
+  $('.decimal-input').change(function validate() {
+    var value = $(this).val();
+    var parsedValue = parseFloat(value.replace(/,/g, '.'));
+
+    if (isNaN(parsedValue) || parsedValue == '') {
+      parsedValue = 0;
+    }
+    $(this).val(parsedValue);
+  });
+
+  $('.woocommerce_hipayenterprise_methods_countries').multi({
+    enable_search: false,
+    non_selected_header: hipay_config_i18n.available_countries,
+    selected_header: hipay_config_i18n.authorized_countries
+  });
+
+  $(document).ready(function () {
+    function toggleFields(merchantId) {
+      [
+        'buttonColor',
+        'buttonShape',
+        'buttonLabel',
+        'buttonHeight',
+        'bnpl'
+      ].forEach(function (fieldId) {
+        var field = $(
+          '#woocommerce_hipayenterprise_methods_' + fieldId + 'paypal'
+        );
+        if (merchantId === '') {
+          field.addClass('readonly');
         } else {
-            $(".directPost_page_config").removeClass("hidden").show();
-            $(".hosted_page_config").hide();
+          field.removeClass('readonly');
         }
-        return false;
-    });
+      });
+    }
 
-    $("ul.nav-tabs > li > a").on("shown.bs.tab", function(e) {
-        var id = $(e.target).attr("href").substr(1);
-        window.location.hash = id;
-    });
+    var merchantIdInput = $(
+      '#woocommerce_hipayenterprise_methods_merchantIdpaypal'
+    );
+    if (merchantIdInput.length) {
+      // Call toggleFields initially to set the correct state on page load
+      toggleFields(merchantIdInput.val());
 
-    var hash = window.location.hash;
-    $('#hipay-container-admin ul a[href="' + hash + '"]').tab('show');
-
-    $(".decimal-input").change(function validate() {
-        var value = $(this).val();
-        var parsedValue = parseFloat(value.replace(/,/g, "."));
-
-        if (isNaN(parsedValue) || parsedValue === "") {
-            parsedValue = 0;
-        }
-        $(this).val(parsedValue);
-    });
-
-    $('.woocommerce_hipayenterprise_methods_countries').multi({
-        enable_search: false,
-        non_selected_header: hipay_config_i18n.available_countries,
-        selected_header: hipay_config_i18n.authorized_countries
-    });
-
-    $(document).ready(function() {
-        function toggleFields() {
-            var merchantId = $('#woocommerce_hipayenterprise_methods_merchantIdpaypal').val();
-            var fields = [
-                'buttonColor',
-                'buttonShape',
-                'buttonLabel',
-                'buttonHeight',
-                'bnpl'
-            ];
-
-            fields.forEach(function(field) {
-                var fieldElement = $('#woocommerce_hipayenterprise_methods_' + field + 'paypal');
-
-                if (merchantId === '') {
-                    fieldElement.addClass('readonly').on('mousedown', preventInteraction);
-                    if (field === 'bnpl') {
-                        fieldElement.prop('checked', false).on('click', preventInteraction);
-                    }
-                } else {
-                    fieldElement.removeClass('readonly').off('mousedown', preventInteraction);
-                    if (field === 'bnpl') {
-                        fieldElement.off('click', preventInteraction);
-                    }
-                }
-            });
-        }
-
-        function preventInteraction(event) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
-        }
-
-        // Run the function when the page loads
-        toggleFields();
-
-        // Attach the function to the input event
-        $('#woocommerce_hipayenterprise_methods_merchantIdpaypal').on('input', toggleFields);
-    });
-
+      merchantIdInput.on('input', function (e) {
+        toggleFields(e.target.value);
+      });
+    }
+  });
 });
