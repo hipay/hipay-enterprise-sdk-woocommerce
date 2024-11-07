adf59cb37f73f42f0e52d2b1ab9e455ba94f8beb
Adam BRECHET <abrechet@hipay.com>
2024-11-07 17:53:21 +0100

    Update color

--- src/woocommerce_hipayenterprise/includes/frontend/template/force-cvv-oc.php
+++ src/woocommerce_hipayenterprise/includes/frontend/template/force-cvv-oc.php
@@ -23,8 +23,8 @@
                        placeholder="<?php _e('CVC', "hipayenterprise"); ?>"/>
 
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512"
-                     tabindex="-1" color="#00ADE9" height="15px" width="15px" class="oneclick-cvv-help-button"
-                     style="color: rgb(0, 173, 233); margin-right: 8px; cursor: pointer;">
+                     tabindex="-1" color="#A50979" height="15px" width="15px" class="oneclick-cvv-help-button"
+                     style="margin-right: 8px; cursor: pointer;">
                     <path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"></path>
                 </svg>
             </div>
