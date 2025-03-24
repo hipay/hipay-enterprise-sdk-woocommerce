ae4dc431b45cbb642132b6eea7d000f1d033b421
Hicham BOUMMAAIZ <hboummaaiz.ext@hipay.com>
2025-03-24 15:40:08 +0100

    Delete sdk multibanco js in email

--- src/woocommerce_hipayenterprise/includes/frontend/template/multibanco.php
+++ src/woocommerce_hipayenterprise/includes/frontend/template/multibanco.php
@@ -1,19 +1,68 @@
-<div style="background-color: #f8f8f8; padding:24px; margin-bottom: 24px">
-    <div id="referenceToPay"></div>
-    <script src='<?php echo $sdkJsUrl ?>'></script>
-    <script type="text/javascript">
-    var hipaySdk = new HiPay({
-        username: 'hosted',
-        password: 'hosted',
-        environment: 'production',
-        lang: '<?php echo substr(get_locale(), 0, 2) ?>'
-    });
-    hipaySdk.createReference('multibanco', {
-        selector: 'referenceToPay',
-        reference: '<?php echo $reference ?>',
-        entity: '<?php echo $entity ?>',
-        amount: '<?php echo $amount ?>',
-        expirationDate: '<?php echo $expirationDate ?>',
-    });
-    </script>
+<style>
+    .multibanco-tpl table {
+        margin-bottom: 0;
+    }
+</style>
+<div class="multibanco-tpl">
+    <table cellspacing="0" cellpadding="0" border="0" style="width: 100%; max-width: 400px; border-collapse: collapse;">
+        <!-- Logo -->
+        <tr>
+            <td style="padding: 15px 10px 15px 0;">
+                <table cellspacing="0" cellpadding="0" border="0">
+                    <tr>
+                        <td style="width: 50%; color: #8a7d63; font-size: 13px; font-weight: bold; vertical-align: top;">
+                            <img src="https://support.hipay.com/hc/article_attachments/21021176698898" alt="MULTIBANCO" style="max-width: 80px;"></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- Entity -->
+        <tr>
+            <td style="padding: 5px 0;">
+                <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;">
+                    <tr>
+                        <td style="width: 50%; color: #8a7d63; font-size: 13px; font-weight: bold; vertical-align: top;">ENTITY:</td>
+                        <td style="color: #333333; font-size: 14px; font-weight: bold; padding-left: 15px;"><?php echo $entity ?></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- Reference -->
+        <tr>
+            <td style="padding: 5px 0;">
+                <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;">
+                    <tr>
+                        <td style="width: 50%; color: #8a7d63; font-size: 13px; font-weight: bold; vertical-align: top;">REFERENCE:</td>
+                        <td style="color: #333333; font-size: 14px; font-weight: bold; padding-left: 15px;"><?php echo $reference ?></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- Amount -->
+        <tr>
+            <td style="padding: 5px 0;">
+                <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;">
+                    <tr>
+                        <td style="width: 50%; color: #8a7d63; font-size: 13px; font-weight: bold; vertical-align: top;">AMOUNT:</td>
+                        <td style="color: #333333; font-size: 14px; font-weight: bold; padding-left: 15px;"><?php echo $amount ?> €</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- Expiration Date -->
+        <tr>
+            <td style="padding: 5px 0;">
+                <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;">
+                    <tr>
+                        <td style="width: 50%; color: #8a7d63; font-size: 13px; font-weight: bold; vertical-align: top;">EXPIRATION DATE:</td>
+                        <td style="color: #333333; font-size: 14px; font-weight: bold; padding-left: 15px;"><?php echo $expirationDate ?></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </div>
