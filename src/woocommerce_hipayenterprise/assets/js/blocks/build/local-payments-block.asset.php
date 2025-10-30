45302a3f337b3696f4c36efb3133f65ac994822b
Hicham BOUMMAAIZ <hboummaaiz.ext@hipay.com>
2025-10-30 14:52:33 +0100

    Add WooCommerce 9 Blocks checkout support ' ;

---
+++ src/woocommerce_hipayenterprise/assets/js/blocks/build/local-payments-block.asset.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'dependencies' => array(
+        'wp-element',
+        'wp-i18n',
+        'wp-html-entities',
+        'wc-blocks-registry',
+        'wc-settings',
+    ),
+    'version' => '2.9.0'
+);
+
